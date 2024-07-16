71141e13ff20c0ead3f606a02cd418b6596d1f23
bienfait-ijambo <ijamboizuba20@gmail.com>
2024-07-16 16:31:49 +0300

    fix error

--- src/views/pages/admin/components/HeaderNav.vue
+++ src/views/pages/admin/components/HeaderNav.vue
@@ -1,6 +1,6 @@
 <template>
       <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
-    <a tested-appname="app"  class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Budget trackr</a>
+    <a tested-appname="app"  class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Budget tracker</a>
 
     <ul class="navbar-nav flex-row d-md-none">
       <li class="nav-item text-nowrap">
