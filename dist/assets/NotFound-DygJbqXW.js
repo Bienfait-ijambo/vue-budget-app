28b6475cb868c55900ffb7829f759e7b9b62f5df
bienfait-ijambo <ijamboizuba20@gmail.com>
2024-08-22 23:12:17 +0300

    add ci-cd workflows

---
+++ dist/assets/NotFound-DygJbqXW.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{o as e,c as t,e as c}from"./index-CaRcDxTW.js";const n={},s=c("h1",null,"Not Found : 404",-1),r=[s];function _(a,d){return e(),t("div",null,r)}const i=o(n,[["render",_]]);export{i as default};
